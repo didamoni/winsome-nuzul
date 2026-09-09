70e00152aa2441b0a82b00be4503628d827ce27b
Mahmoud Al-Didamoni <mahmoudaldidamoni@gmail.com>
2026-09-09 09:17:58 +0300

    feat: create app viewmodels

---
+++ app/src/main/java/com/winsome/nuzul/presentation/screen/favorites/FavoritesViewModel.kt
@@ -0,0 +1,37 @@
+package com.winsome.nuzul.presentation.screen.favorites
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FavoritesViewModel @Inject constructor(
+    // private val useCase: Unit
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(Unit)
+    val state = _state.asStateFlow()
+
+    /* Events
+	private val _event = MutableSharedFlow<Unit>()
+	val event = _event.asSharedFlow()
+	 */
+
+    init {
+        viewModelScope.launch { }
+    }
+
+    /* Intents
+	fun processIntent(intent: Unit) {
+		when (intent) {
+			Unit -> doSomething()
+		}
+	}
+
+	private fun doSomething() = viewModelScope.launch {  }
+	 */
+}
